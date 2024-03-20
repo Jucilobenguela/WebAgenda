0014968cd02a889c864a83a8fea143e93bc203b5
Jucilobenguela <Jucilobenguela@hotmail.com>
2024-03-20 22:01:28 +0000

    add necessary denpendencies

---
+++ WebAgenda_API/src/main/java/com/benguela/WebAgenda_API/WebAgendaApiApplication.java
@@ -0,0 +1,13 @@
+package com.benguela.WebAgenda_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAgendaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAgendaApiApplication.class, args);
+	}
+
+}
